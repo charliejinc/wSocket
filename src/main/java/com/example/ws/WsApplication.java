d1a9ae3a6524e5aa65dc3a20b6cfa83aa151e61a
金成 <jincheng2149@126.com>
2023-05-15 13:41:56 +0800

    版本升级

--- src/main/java/com/example/ws/WsApplication.java
+++ src/main/java/com/example/ws/WsApplication.java
@@ -3,7 +3,6 @@ package com.example.ws;
 import com.example.ws.annotation.EnableSocketIo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableSocketIo
